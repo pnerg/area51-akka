40c2362aa505a1751d511e1534d3632e881d6e95
Peter Nerg <peter.nerg@gmail.com>
2016-12-02 11:24:00 +0100

    consolidated the member test code to one file

--- src/main/scala/org/dmonix/area51/akka/cluster/ClusterMember.scala
+++ src/main/scala/org/dmonix/area51/akka/cluster/ClusterMember.scala
@@ -1,13 +1,17 @@
 package org.dmonix.area51.akka.cluster
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
-import akka.cluster.Cluster
+import akka.cluster.{Cluster, Member}
 import akka.cluster.ClusterEvent._
+
+import scala.collection.mutable
 
 /**
   * @author Peter Nerg.
   */
 class ClusterMember(cluster:Cluster) extends Actor with ActorLogging  {
+
+  private var members = mutable.ListBuffer[Member]()
 
   // subscribe to cluster changes, re-subscribe when restart
   override def preStart(): Unit = {
@@ -15,16 +19,27 @@ class ClusterMember(cluster:Cluster) extends Actor with ActorLogging  {
       classOf[MemberEvent], classOf[UnreachableMember])
   }
 
-  override def postStop(): Unit = cluster.unsubscribe(self)
+  override def postStop(): Unit = {
+    cluster.unsubscribe(self)
+  }
+
   def receive = {
     case MemberUp(member) =>
-      log.info(s"Member is Up: [${member.address} with roles [${member.roles}]]")
+      members += member
+      log.info(s"Member is Up: [${member.address} with roles [${member.roles}]], now active members [{$sortedMembers}]")
+
     case UnreachableMember(member) =>
-      log.info("Member detected as unreachable: {}", member)
+      log.info(s"Member detected as unreachable: [$member]")
     case MemberRemoved(member, previousStatus) =>
-      log.info("Member is Removed: {} after {}", member.address, previousStatus)
+      log.info(s"Member is Removed: [{$member.address}] after [$previousStatus], now active members [{$sortedMembers}]")
+      members -= member
     case _: MemberEvent => // ignore
   }
+
+  private def sortedMembers = {
+      members.sorted(Member.ageOrdering).map(_.address)
+  }
+
 }
 
 object StartClusterMember extends App with ClusterSettings {
@@ -35,7 +50,30 @@ object StartClusterMember extends App with ClusterSettings {
   //join in to the cluster
   cluster.joinSeedNodes(seedNodes)
 
-  actorSystem.actorOf(Props(new ClusterMember(cluster)))
-  actorSystem.actorOf(Props(new ClusterMember(cluster)))
+  actorSystem.actorOf(Props(new ClusterMember(cluster)), "ClusterMember")
+
+  sys.ShutdownHookThread {
+    println("Starting Shutdown")
+    cluster.leave(cluster.selfAddress)
+    Thread.sleep(10000)
+    println("Finished Shutdown")
+  }
+}
+
+object StartClusterSeed extends App with ClusterSettings {
+  val actorSystem = ActorSystem(actorSystemName, Configuration.seedCfg(6969))
+
+  val cluster = Cluster(actorSystem)
+
+  //join in to the cluster
+  cluster.joinSeedNodes(seedNodes)
+
+  actorSystem.actorOf(Props(new ClusterMember(cluster)), "ClusterMember")
+
+  sys.ShutdownHookThread {
+    println("Starting Shutdown")
+    cluster.leave(cluster.selfAddress)
+    println("Finished Shutdown")
+  }
 
 }
