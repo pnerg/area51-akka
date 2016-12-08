d0b7de2d0ed036ec7a4e0c36517e9458acd31dc0
Peter Nerg <peter.nerg@gmail.com>
2016-12-08 11:16:08 +0100

    added example on how to wait for an actor to start

---
+++ src/main/scala/org/dmonix/area51/akka/AwaitActorStartupExample.scala
@@ -0,0 +1,84 @@
+package org.dmonix.area51.akka
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+/**
+  * A dummy actor that has a configurable delay for the start phase
+  * @param startDelay The
+  * @author Peter Nerg
+  */
+class SlowStartActorActor(startDelay:FiniteDuration) extends Actor with ActorLogging {
+
+  log.info("Create")
+
+  override def preStart(): Unit = {
+    log.info(s"PreStart [$startDelay]")
+    Thread.sleep(startDelay.toMillis)
+    log.info("PreStarted")
+  }
+
+  override def receive: Receive = {
+    case a: Any => log.info("PlayAroundActor:" + a)
+  }
+
+}
+
+/**
+  * Simple example on how one can go about to verify that an actor (ActorRef) exists and is fully started.
+  * Use case could be when bootstrapping a number of actors that potentially have long start phases.
+  * @author Peter Nerg
+  */
+object AwaitActorStartup extends App {
+  val as = ActorSystem("AwaitActorStartup")
+//  val x = (Random.nextInt(3000).millis
+
+  //play around with the start delay and wait time to get various results
+  val ref1 = as.actorOf(Props(new SlowStartActorActor(3.seconds)), "SlowStartActorActor-1")
+  val ref2 = as.actorOf(Props(new SlowStartActorActor(0.seconds)), "SlowStartActorActor-2")
+  val ref3 = as.actorOf(Props(new SlowStartActorActor(2.seconds)), "SlowStartActorActor-3")
+  waitForActors(Seq(ref1, ref2, ref3))(2.seconds)
+
+  //here only to cut the actor system after the above tests have been performed
+  Await.ready(as.terminate(), 15.seconds)
+
+  private def waitForActors(actors: Seq[ActorRef])(implicit waitTime:FiniteDuration) = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+
+    //results in a sequence with Future[Boolean]
+    val resolveFutures = actors.
+      map(resolve(_)). //maps each ActorRef by performing a resolve op
+      map(isSuccess(_)) //maps the result of the resolve op to Boolean
+
+    //results in a single Future[Boolean]
+    val totalResult = Future.sequence(resolveFutures) //sequences the Seq[Future[Boolean]] to a single future with Seq[Boolean]
+      .map(result => result.forall(_ == true)) //maps/reduces the Seq[Boolean] to a single boolean
+
+    //double the wait time for the Future so we don't time out here
+    //this timeout will never anyways be reached, it's the resolveOne timeout that dictates
+    if(Await.result(totalResult, waitTime*2)) {
+      println("Successfully resolved all actors")
+    }
+    else {
+      println("Failed to resolve one or more actors")
+    }
+  }
+
+  private def isSuccess(f:Future[ActorRef])(implicit ec:ExecutionContext):Future[Boolean] = {
+    f.map(ref => {
+      println(s"Resolved actor [${ref.path}]")
+      true
+    }).recover {case ex:Throwable =>
+      println(s"Failed to resolve actor within specified time [${ex.getMessage}]")
+      false
+    }
+  }
+
+  private def resolve(actor:ActorRef)(implicit waitTime:FiniteDuration):Future[ActorRef] = {
+    as.actorSelection(actor.path).resolveOne(waitTime)
+  }
+
+}
